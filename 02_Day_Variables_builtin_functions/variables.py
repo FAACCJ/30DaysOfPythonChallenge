8f363c8e52a74c84dee863939b71a7cb85784a25
FAACCJ <casonjorden5648@yahoo.com>
2025-07-13 16:01:32 -0400

    Update variables.py
    practice

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,40 +1,142 @@
+last_name = 'jorden'
+full_name = 'cason', 'jorden'
+age = 30
+country = 'usa'
+year = 2025
+is_true = true
 
-# Variables in Python
+is_true = true
 
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
-country = 'Finland'
-city = 'Helsinki'
-age = 250
-is_married = True
-skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
-person_info = {
-    'firstname':'Asabeneh', 
-    'lastname':'Yetayeh', 
-    'country':'Finland',
-    'city':'Helsinki'
-    }
+is_true = True
+is_light_on = True
+print(type(first_name))
+<class 'str'>
+print(len(first_name))
+5
+print(compare(len(first_name, last_name)))
 
-# Printing the values stored in the variables
+len(first_name) > len(last_name)
+False
+num_one = 5
+num_two = 4
+print(num_one + num_two)
+9
+num_one + num_two = sum_of_num_1_and_2
 
-print('First name:', first_name)
-print('First name length:', len(first_name))
-print('Last name: ', last_name)
-print('Last name length: ', len(last_name))
-print('Country: ', country)
-print('City: ', city)
-print('Age: ', age)
-print('Married: ', is_married)
-print('Skills: ', skills)
-print('Person information: ', person_info)
+(num_one + num_two) = sum_of_nums
 
-# Declaring multiple variables in one line
+total = (num_one + num_ two)
 
-first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+(num_one + num_two) == sum_of_nums
 
-print(first_name, last_name, country, age, is_married)
-print('First name:', first_name)
-print('Last name: ', last_name)
-print('Country: ', country)
-print('Age: ', age)
-print('Married: ', is_married)
+sum_of_nums = (num_one = num_two)
+
+int total = num_one + num_two
+
+int('total' = (num_one + num_two))
+
+total = num_one + num_two
+total
+9
+num_one - num_two
+1
+subtract_total = num_one - num_two
+subtract_total
+1
+total
+9
+num_one * num_two
+20
+diff = num_one * num_two
+diff
+20
+total
+9
+subtract_total
+1
+num_one / num_two
+1.25
+division = num_one / num_two
+division
+1.25
+num_one % num_two
+1
+remainder = num_one % num_two
+remainder
+1
+num_one // num_two
+1
+floor_division = num_one // num_two
+florr_division
+
+floor_division
+1
+circle_radius = print('what is the radius of a circle?: )
+                      
+
+circle_radius = input(' what is the radius of a circle?: ')
+                      
+ what is the radius of a circle?: 30
+area_of_circle = 2827.43338823
+                      
+circum_of_circle = 188.495559215
+                      
+circle_radius * circle_radius * 3.14
+                      
+
+circle_radius * circle_radius
+                      
+
+print(circle_radius * circle_radius)
+                      
+
+print(int(circle_radius * circle_radius))
+                      
+
+print(str(circle_radius * circle_radius))
+                      
+
+num_one + num_two
+                      
+9
+circle_radius * circle_radius
+                      
+
+circle_int_rad = int(circle_radius)
+                      
+print('area of circle = ', int(circle_int_rad * circle_int_rad))
+                      
+area of circle =  900
+area of circle =  900
+                      
+
+print('area of circle = ', int(circle_int_rad * circle_int_rad * 3.14))
+                      
+area of circle =  2826
+print('cum_circle = ', int(2 * 3.14 * circle_int_rad))
+                      
+cum_circle =  188
+first_name = input('what is Your first name?: ')
+                      
+what is Your first name?: Cason
+last_name = input('what is your last name?: ')
+                      
+what is your last name?: jorden
+country = input('what country do you live in? ')
+                      
+what country do you live in? usa
+age = ('how old are you?: ')
+                      
+age = input('how old are you?: ')
+                      
+how old are you?: 90
+person_info = print(first_name, last_name, age, country)
+                      
+Cason jorden 90 usa
+person_info
+                      
+person_info = (first_name, last_name, age, country)
+                      
+person_info
+                      
+('Cason', 'jorden', '90', 'usa')
